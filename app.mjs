642b2eee4a8983efc9d8e6b1f1fa5065fa42a104
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-07 04:04:00 +0330

    Creat .env file and add it to app js for reading links.txt file path

--- app.mjs
+++ app.mjs
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import generator from "./src/generate.mjs";
 import fetchFile from "./src/readfile.mjs";
 
-const filePath = "./links.txt";
+const filePath = process.env.LINKS_FILE_PATH;
 
 async function start() {
   let data = await fetchFile(filePath);
